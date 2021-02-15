44c6914c5e90b484ff2c708bbbc1b9c353c342ad
JesperKYH <jesper.pettersson@student.kyh.se>
2021-02-15 09:08:56 +0100

    init commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+      <h1>Pictures Portfolio</h1>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    nav{
+        border: 1px solid black;
+        background-color: #f8f1f1;
+    }
+</style>
